74c4382b73dc12bb4a625b940d4fa876a9f8e0f9
proxyteng <tengjuchi@gmail.com>
2022-04-15 02:13:08 +0200

    added lionicon and a favicon

---
+++ assets/iconset-store.js
@@ -0,0 +1,6 @@
+import search from "./store/search.svg.js"
+import basket from "./store/basket.svg.js"
+import basketMinus from "./store/basket-minus.svg.js"
+import basketPlus from "./store/basket-plus.svg.js"
+
+export { search, basket, basketMinus, basketPlus }
